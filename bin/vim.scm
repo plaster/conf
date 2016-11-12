23fa12e2956a3fbbc81554ca0ec719c55945c540
Haruka Kai <plalal@gmail.com>
2016-11-13 04:34:18 +0900

    wip: gauche keyword generator

--- bin/vim.scm
+++ bin/vim.scm
@@ -6,16 +6,17 @@
 
 (define (parse-line line)
   (match (port->sexp-list (open-input-string line))
-    [ (s (m)) (values s m) ]
+    [ (y (m)) (values y m) ]
     [else
       (errorf "unknown output: ~s" line)
       ]
     ))
 
-(define (emit-line s m)
-  (let1 v (eval s (find-module m))
+(define (emit-line y m)
+  (let1 v (eval y (find-module m))
     (and
-      (rxmatch-case (symbol->string s)
+      (rxmatch-case (symbol->string y)
+        [ #/ / () #f ]
         [ #/^let/ () #t ]
         [ #/-let1$/ () #t ]
         [ #/^define/ () #t ]
@@ -23,19 +24,19 @@
         [ #/-case$/ () #t ]
         [else #f]
         )
-      (print "set lw+=" s)
+      (print "set lw+=" y)
       )
 
     (cond
       [ (procedure? v)
-       (format #t "syn keyword schemeExtFunc ~a~%" s)
+       (format #t "syn keyword schemeExtFunc ~a~%" y)
        ]
       [ (or (is-a? v <macro>)
             (is-a? v <syntax>))
-       (format #t "syn keyword schemeExtSyntax ~a~%" s)
+       (format #t "syn keyword schemeExtSyntax ~a~%" y)
        ]
       [else
-        (format #t "syn keyword schemeExtSyntax ~a~%" s)
+        (format #t "syn keyword schemeExtSyntax ~a~%" y)
         ]
       )))
 
